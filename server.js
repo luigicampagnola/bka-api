380c9f2d34244148adaff79a99463eb45a76bd1d
luigicampagnola <58444283+luigicampagnola@users.noreply.github.com>
2021-05-26 13:19:16 +0200

    register profile/id transactions

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const database = {
         movements: [
           {
             id: 0,
-            Type: "Deposit",
+            Type: "Withdrawal",
             Date: "05/23/2021 at 17:11",
             Amount: 0,
           },
@@ -43,7 +43,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("this is working");
+  res.send(database.users);
 });
 
 app.post("/signin", (req, res) => {
@@ -53,20 +53,61 @@ app.post("/signin", (req, res) => {
   ) {
     res.json("success");
   } else {
-      res.status(400).json('error logging in')
+    res.status(400).json("error logging in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    joined: new Date(),
+    transactions: {
+      movements: [
+        {
+          id: 0,
+          Type: "Deposit",
+          Date: "05/23/2021 at 17:11",
+          Amount: 0,
+        },
+      ],
+    },
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.post("/transactions", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user.transactions);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
   }
 });
 
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
-
-/* 
-  / Root--> res = this is working
-  /signin --> POST  = success or fail
-  /register --> POST = user
-  /profile/:userId ---> GET = user
-  /transactions --> PUT --> user
-
-*/
 
