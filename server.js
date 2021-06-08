af0bd5b90ca1cd01bd6012eae1a1647096d10375
luigicampagnola <58444283+luigicampagnola@users.noreply.github.com>
2021-06-08 19:02:32 +0200

    added signin controller

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
 const register = require("./contollers/register");
+const signin = require("./contollers/signin");
 
 const db = knex({
   client: "pg",
@@ -35,26 +36,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  const { email, password } = req.body;
-  db.select("email", "hash")
-    .from("login")
-    .where("email", "=", email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(password, data[0].hash);
-      if (isValid === true) {
-        return db
-          .select("*")
-          .from("users")
-          .where("email", "=", email)
-          .then((user) => {
-            res.json(user[0]);
-          })
-          .catch((err) => res.status(400).json("unable to get user"));
-      } else {
-        res.status(400).json("wrong credentials");
-      }
-    })
-    .catch((err) => res.status(400).json("login fail"));
+  signin.signInHandler(req, res, db, bcrypt);
 });
 
 /* 6/5/21 encountered a bug that didn't
