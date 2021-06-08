41532aee21b3e197d4b5116d30c2afc22bd29eb8
luigicampagnola <58444283+luigicampagnola@users.noreply.github.com>
2021-06-08 20:53:27 +0200

    added validation to signin

--- contollers/signin.js
+++ contollers/signin.js
@@ -1,5 +1,8 @@
 const signInHandler = (req, res, db, bcrypt) => {
   const { email, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json("incorrect form submission");
+  }
   db.select("email", "hash")
     .from("login")
     .where("email", "=", email)
