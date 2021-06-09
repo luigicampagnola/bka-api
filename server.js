815394503b4fc2b22daf4aceebe6f2fa27099cf8
luigicampagnola <58444283+luigicampagnola@users.noreply.github.com>
2021-06-09 14:35:00 +0200

    fixednaming

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const transactions = require("./controllers/transactions");
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
 });
